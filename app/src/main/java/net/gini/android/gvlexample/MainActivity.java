fa26ce1c67c4962fd1f8feaf2d060adf4c6f0423
Alpár Szotyori <a.szotyori@gini.net>
2017-11-22 14:58:15 +0100

    Created the main screen
    Issue #3

--- app/src/main/java/net/gini/android/gvlexample/MainActivity.java
+++ app/src/main/java/net/gini/android/gvlexample/MainActivity.java
@@ -1,7 +1,10 @@
 package net.gini.android.gvlexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == R.id.infoItem) {
+            // Launch InfoActivity
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void launchGiniVision(final View view) {
     }
 }
